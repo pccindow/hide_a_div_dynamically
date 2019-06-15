7be744ddc66032e6bb45e484a755a4d3851943b7
pccindow <pccindow78@msn.com>
2019-06-16 00:02:11 +0200

    change to en

--- app/javascript/javascripts/floor_hide.js
+++ app/javascript/javascripts/floor_hide.js
@@ -1,6 +1,6 @@
 $( document ).on('turbolinks:load', function() {
   $('#property_properties_type').change(function() {
-    if ($( this ).val() == 'appartement') {
+    if ($( this ).val() == 'apartment') {
       document.getElementById("hide_floor").style.display = "initial";
     } else {
       document.getElementById("hide_floor").style.display = "none";
--- app/javascript/javascripts/immo_type_autocomplete.js
+++ app/javascript/javascripts/immo_type_autocomplete.js
@@ -1,5 +1,5 @@
 $( document ).on('turbolinks:load', function() {
   return ($("#property_properties_type").autocomplete)({
-  source: ["appartement", "maison"]
+  source: ["apartment", "house"]
   });
 });
